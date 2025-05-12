c8675aaee77967177fcc95d148e48c33167b6284
Alex Gartland <alexgartland22@gmail.com>
2025-05-12 14:46:57 +0100

    Add routing, controller and views for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+#Fill these out with the correct recipe details
+
+#Clean database before running seed file
+puts "Cleaning database..."
+Recipe.destroy_all
+
+puts "Creating new recipes..."
+Recipe.create!(name: "Spaghetti all Carbonara",
+              description: "Carbonara is made with guanciale (cured pork), eggs, Pecorino Romano cheese, spaghetti pasta, and lots of black pepper.",
+              image_url: "https://www.allrecipes.com/thmb/InzCzO3OWS1inqCRU3AJhv33fhg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/245775-spaghetti-alla-carbonara-the-traditional-italian-recipe-DDMFS-4x3-879c32ee3cfb463582e3e6230e311029.jpg",
+              rating: 4.5)
+Recipe.create!(name: "Braised Pork Ragu",
+               description: "A perfect make-ahead dish that is served best over Parmesan risotto, but is also great over pasta or potatoes!",
+               image_url: "https://www.allrecipes.com/thmb/vKYnmmp7zZLj_N5M-iS0HKCzAhg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7983282-braised-pork-ragu-Sugea-4x3-1-168586ca15524e15a7012009776e2235.jpg",
+               rating: 4.2)
+Recipe.create!(name: "Lasagne Verdi al Forno",
+               description: "This lasagne verde made with spinach pasta cannot be beaten.",
+                image_url: "https://www.allrecipes.com/thmb/NZiIlIWZvNJlhqyYvspUr7s9UjM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/421809-686b67e690ae4a40a9e8d588ea692c3c.jpg",
+                rating: 4.1)
+
+puts "#{Recipe.count} recipes created"
