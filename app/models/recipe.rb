cccbf09aabae35369976d43d4b6efc2bae37b279
Alex Gartland <alexgartland22@gmail.com>
2025-05-12 12:18:16 +0100

    Added model validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, numericality: { in: 0..5 }
 end
